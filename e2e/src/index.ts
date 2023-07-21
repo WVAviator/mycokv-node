3621697904ab4442d0a7db71c217314ca5cbf128
Wvaviator <wvaviator@gmail.com>
2023-07-21 11:11:10 -0500

    set up basic communication with db server

--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -1,3 +1,30 @@
-import { MycoKV } from 'mycokv-node';
+import { MycoKV } from "mycokv-node";
 
-const myco = new MycoKV('http://localhost:6922');
+const testBasicCommands = async () => {
+    const myco = await MycoKV.connect("http://127.0.0.1:6922");
+
+    console.log("Testing basic commands...");
+
+    console.log("Testing put...");
+    const put = await myco.put("foo", "bar");
+    console.log(put);
+
+    console.log("Testing get...");
+    const get = await myco.get("foo");
+    console.log(get);
+
+    console.log("Testing delete...");
+    const del = await myco.delete("foo");
+    console.log(del);
+
+    console.log("Testing get after delete...");
+    const get2 = await myco.get("foo");
+    console.log(get2);
+
+    console.log("Testing disconnect...");
+    await myco.disconnect();
+
+    console.log("Basic commands test complete.");
+};
+
+testBasicCommands();
--- src/MycoKV.ts
+++ src/MycoKV.ts
@@ -1,10 +1,94 @@
+import net from "net";
+
 export default class MycoKV {
-    constructor(private connectionString: string) {
-        //TODO: Establish connection to MycoKV database
-        //TODO: Handle connection error with appropriate logs
+    private responseResolver:
+        | ((value: string | PromiseLike<string>) => void)
+        | null = null;
+    private client: net.Socket;
+    private host: string;
+    private port: number;
+
+    private constructor(private connectionString: string) {
+        const [host, port] = connectionString.replace("http://", "").split(":");
+        this.host = host;
+        this.port = parseInt(port);
     }
 
-    //TODO: Implement get method
-    //TODO: Implement put method
-    //TODO: Implement delete method
+    public static async connect(connectionString: string): Promise<MycoKV> {
+        const myco = new MycoKV(connectionString);
+        await myco.establishConnection();
+        myco.addListeners();
+        return myco;
+    }
+
+    private establishConnection(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            try {
+                this.client = net.createConnection(
+                    { host: this.host, port: this.port },
+                    () => {
+                        console.log(
+                            `Successfully connected to MycoKV at ${this.host}:${this.port}`
+                        );
+
+                        setTimeout(resolve, 1000);
+                    }
+                );
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
+        });
+    }
+
+    private addListeners(): void {
+        this.client.on("data", (data) => {
+            if (this.responseResolver) {
+                this.responseResolver(data.toString());
+                this.responseResolver = null;
+            }
+        });
+
+        this.client.on("end", () => {
+            console.log("MycoKV connection terminated.");
+        });
+
+        this.client.on("error", (err) => {
+            console.log(err);
+        });
+    }
+
+    public async get(key: string): Promise<string> {
+        //TODO: Handle nested keys and json return values
+        //TODO: Handle errors
+        return this.sendCommand(`GET ${key}\n`);
+    }
+
+    public async put(key: string, value: string): Promise<string> {
+        //TODO: Handle errors
+        return this.sendCommand(`PUT ${key} "${value}"\n`);
+    }
+
+    public async delete(key: string): Promise<string> {
+        //TODO: Handle errors
+        return this.sendCommand(`DELETE ${key}\n`);
+    }
+
+    public async disconnect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.end(() => {
+                resolve();
+            });
+        });
+    }
+
+    private sendCommand(command: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            if (this.responseResolver) {
+                reject("Already awaiting response");
+            }
+            this.responseResolver = resolve;
+            this.client.write(command);
+        });
+    }
 }
