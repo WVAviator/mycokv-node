b3deb2d5a2574393f1c37443842a43475c56e682
Wvaviator <wvaviator@gmail.com>
2023-07-21 08:38:47 -0500

    initial setup and configuration

---
+++ src/MycoKV.ts
@@ -0,0 +1,10 @@
+export default class MycoKV {
+    constructor(private connectionString: string) {
+        //TODO: Establish connection to MycoKV database
+        //TODO: Handle connection error with appropriate logs
+    }
+
+    //TODO: Implement get method
+    //TODO: Implement put method
+    //TODO: Implement delete method
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as MycoKV } from "./MycoKV";
